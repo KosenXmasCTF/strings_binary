dc4b0b76863455645e55cea51ce035bb40aea32b
Iwancof <rock.iwancof@gmail.com>
2020-12-23 01:29:37 +0900

    Add problem

---
+++ main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main() {
+	puts("Where is flag??");
+	puts("I think flag is on this file...");
+}
+
+char flag[] = "xm4s{strings_binary_is_simple_and_powerful!}";
+
+
---
+++ writeup.md
@@ -0,0 +1,4 @@
+まぁ問題名がヒントになってるし、やるだけですね。
+
+strings ./binary_strings | grep xm4s
+
